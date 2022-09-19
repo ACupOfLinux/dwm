a6bb3d9e63765108e6ab67484c896b5061446b0e
Kayden Harrell <110700997+ACupOfLinux@users.noreply.github.com>
2022-09-19 14:24:40 -0400

    Add files via upload

---
+++ patch/include.h
@@ -0,0 +1,31 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_statuscolors.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "autostart.h"
+#include "cyclelayouts.h"
+#include "movestack.h"
+#include "pertag.h"
+#include "restartsig.h"
+#include "scratchpad.h"
+#include "transfer.h"
+#include "vanitygaps.h"
+#include "warp.h"
+/* Layouts */
+#include "layout_centeredmaster.h"
+#include "layout_columns.h"
+#include "layout_fibonacci.h"
+#include "layout_gapplessgrid.h"
+#include "layout_grid.h"
+#include "layout_horizgrid.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
